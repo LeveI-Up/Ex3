320e179d97f2e066fe2827ba4c24f223c1c63053
Saar <saar9550@gmail.com>
2021-11-02 11:34:25 +0200

    Add all filles

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+
+
+    [SerializeField] int movespeed=1;
+    [SerializeField] Rigidbody2D playerRigidBody;
+    [SerializeField] Animator playerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+        playerRigidBody.velocity = new Vector2(horizontalMovement, verticalMovement) * movespeed;
+
+        playerAnimator.SetFloat("movementX", playerRigidBody.velocity.x);
+        playerAnimator.SetFloat("movementY", playerRigidBody.velocity.y);
+
+        if(horizontalMovement == 1 || horizontalMovement == -1 || verticalMovement == 1 || verticalMovement == -1)
+        {
+            playerAnimator.SetFloat("lastX", horizontalMovement);
+            playerAnimator.SetFloat("lastY", verticalMovement);
+
+        }
+
+
+    }
+}
